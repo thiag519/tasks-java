b671aa1babc47c2e43e21b17b256686aac43efdf
thiago souza <thiagosouzadasilva78@gmail.com>
2026-05-02 10:41:58 -0300

    feat: add SecurityConfig.java

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.example.demo.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+      .cors(cors -> {}) // ativa CORS
+      .csrf(csrf -> csrf.disable()) // desativa CSRF (API REST)
+      .authorizeHttpRequests(auth -> auth
+        .requestMatchers("/tasks/**").permitAll() // libera suas rotas
+        .anyRequest().authenticated()
+      );
+
+    return http.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    configuration.setAllowedOrigins(Arrays.asList(
+      "http://127.0.0.1:5500", // seu front local
+      "http://localhost:5500",
+      "https://tasks-front-dusky.vercel.app" // seu front deploy
+    ));
+
+    configuration.setAllowedMethods(Arrays.asList(
+      "GET", "POST", "PUT", "DELETE", "OPTIONS"
+    ));
+
+    configuration.setAllowedHeaders(Arrays.asList(
+      "*"
+    ));
+
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+
+    return source;
+  }
+}
