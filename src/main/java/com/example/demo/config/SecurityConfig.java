afe59844cb0836801c6c02bcae136aadf9cf6381
thiago souza <thiagosouzadasilva78@gmail.com>
2026-05-02 11:13:10 -0300

    fix: ajustes em SecurityConfig.java

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -4,10 +4,9 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -18,10 +17,11 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
-      .cors(cors -> {}) // ativa CORS
+      .cors(org.springframework.security.config.Customizer.withDefaults())// ativa CORS
       .csrf(csrf -> csrf.disable()) // desativa CSRF (API REST)
       .authorizeHttpRequests(auth -> auth
         .requestMatchers("/tasks/**").permitAll() // libera suas rotas
+        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
         .anyRequest().authenticated()
       );
 
@@ -43,7 +43,10 @@ public class SecurityConfig {
     ));
 
     configuration.setAllowedHeaders(Arrays.asList(
-      "*"
+      "Authorization",
+      "Content-Type",
+      "Accept",
+      "x-admin-password"
     ));
 
     configuration.setAllowCredentials(true);
